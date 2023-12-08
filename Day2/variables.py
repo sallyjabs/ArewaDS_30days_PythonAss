80b8f9e8c774d88b75229e6fa714e1df29399557
unknown <sallome64@gmail.com>
2023-12-08 11:41:56 +0100

    second commit for day2

--- Day2/variables.py
+++ Day2/variables.py
@@ -37,17 +37,18 @@ print(len(last_name))
 #Declare 5 as num_one and 4 as num_two
 num_one = 5
 num_two = 4
+
 total = num_one + num_two
-diff = num_one - num_two
+diff = num_two - num_one
 product = num_one * num_two
-division = num_one / num_two
+division = num_two / num_one
 reminder = num_two % num_one
-exp = num_one ^ num_two
+exp = num_one ** num_two
 floor_division = num_one // num_two
 
 #The radius of a circle is 30 meters
 #Calculate the area of a circle and assign the value to a variable name of area_of_circle
-area_of_circle = 3.14 * 30 ^2
+area_of_circle = 3.14 * 30 ** 2
 
 #Calculate the circumference of a circle and assign the value to a variable name of circum_of_circle
 circum_of_circle = 2 * 3.14 * 30 
